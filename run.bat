a2088380475b7e9f527d1511b32e0c002b224074
crmulent <kingcapacio47@gmail.com>
2025-08-04 23:33:32 +0800

    Update run.bat to compile all .cpp files in src along with main.cpp

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
-REM Compile main.cpp to main.exe using g++
-g++ main.cpp -o main.exe
+REM Compile all .cpp files in src and main.cpp to main.exe using g++
+g++ src\*.cpp main.cpp -o main.exe -std=c++17 -pthread
 REM Run the compiled main.exe file
 main.exe
